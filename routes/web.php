e8be9bd80c03f608d2ebaa6a10679cddfe8723b5
yulicahyani <yulicahyani1101@gmail.com>
2022-05-11 19:51:39 +0800

    create navbar, sidebar, dashboard

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,57 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+/* Testing */
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin\dashboard');
 });
+
+
+/* Login */
+Route::get('/login', function () {
+    return view('login');
+});
+
+/* Admin */
+
+Route::get('/dashboard', function () {
+    return view('admin\dashboard');
+});
+
+Route::get('/new-user', function () {
+    return view('admin\new-user');
+});
+
+Route::get('/data-pegawai', function () {
+    return view('admin\data-pegawai');
+});
+
+Route::get('/customer', function () {
+    return view('admin\customer');
+});
+
+Route::get('/pemesanan', function () {
+    return view('admin\pemesanan');
+});
+
+Route::get('/penjualan-baru', function () {
+    return view('admin\penjualan-baru');
+});
+
+Route::get('/jumlah-penjualan', function () {
+    return view('admin\jumlah-penjualan');
+});
+
+Route::get('/target', function () {
+    return view('admin\target');
+});
+
+Route::get('/pengiriman', function () {
+    return view('admin\pengiriman');
+});
+
+Route::get('/penjadwalan', function () {
+    return view('admin\penjadwalan');
+});
+
