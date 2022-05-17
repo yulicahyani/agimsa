1038c08f2c1991bbfaef66965345ab143c7944ff
arisurya7 <arisurya425@gmail.com>
2022-05-17 13:44:59 +0800

    Updated dashboard controller

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,91 +13,110 @@ class DashboardController extends Controller
 {
 
     public function index(Request $request){
-        $penjualanperbulan = [];
-        foreach(range(1,12) as $bulan) {
-            $penjualanperbulan[] = Penjualan::whereMonth('tgl_penjualan', '=', date($bulan))->whereYear('tgl_penjualan', '=', date('Y'))->count();
-        }
-
-        $data = [
-            'title'=>'Dashboard',
-            'countUser'=> Pegawai::all()->count(),
-            'countPenjualan'=> Penjualan::all()->count(),
-            'tanggal' => date('M d, Y'),
-            'countCustomer'=> Customer::all()->count(),
-            'countPenjualanBulanan'=> Penjualan::whereMonth('tgl_penjualan', '=', date('m'))->whereYear('tgl_penjualan', '=', date('Y'))->count(),
-            'countPendapatanBulanan'=> Penjualan::whereMonth('tgl_penjualan', '=', date('m'))->whereYear('tgl_penjualan', '=', date('Y'))->sum('total_harga'),
-            'countPendapatan'=> Penjualan::all()->sum('total_harga'),
-            'countProduk'=> Barang::all()->count(),
-            'penjualanperbulan' => $penjualanperbulan,
-        ];
+        if(session()->has('user') && session('user')->jabatan == 'Admin'){
+            $penjualanperbulan = [];
+            foreach(range(1,12) as $bulan) {
+                $penjualanperbulan[] = Penjualan::whereMonth('tgl_penjualan', '=', date($bulan))->whereYear('tgl_penjualan', '=', date('Y'))->count();
+            }
     
-        return view('admin.dashboard',$data);
+            $data = [
+                'title'=>'Dashboard',
+                'countUser'=> Pegawai::all()->count(),
+                'countPenjualan'=> Penjualan::all()->count(),
+                'tanggal' => date('M d, Y'),
+                'countCustomer'=> Customer::all()->count(),
+                'countPenjualanBulanan'=> Penjualan::whereMonth('tgl_penjualan', '=', date('m'))->whereYear('tgl_penjualan', '=', date('Y'))->count(),
+                'countPendapatanBulanan'=> Penjualan::whereMonth('tgl_penjualan', '=', date('m'))->whereYear('tgl_penjualan', '=', date('Y'))->sum('total_harga'),
+                'countPendapatan'=> Penjualan::all()->sum('total_harga'),
+                'countProduk'=> Barang::all()->count(),
+                'penjualanperbulan' => $penjualanperbulan,
+            ];
+        
+            return view('admin.dashboard',$data);
+        }else{
+            return redirect('/login');
+        }
+       
     }
 
     public function index_sales(Request $request){
-        $penjualanperbulan = [];
-        foreach(range(1,12) as $bulan) {
-            $penjualanperbulan[] = Penjualan::whereMonth('tgl_penjualan', '=', date($bulan))->whereYear('tgl_penjualan', '=', date('Y'))->count();
-        }
+        if(session()->has('user') && session('user')->jabatan == 'Sales'){
+            $penjualanperbulan = [];
+            foreach(range(1,12) as $bulan) {
+                $penjualanperbulan[] = Penjualan::whereMonth('tgl_penjualan', '=', date($bulan))->whereYear('tgl_penjualan', '=', date('Y'))->count();
+            }
 
-          
-        $data = [
-            'title'=>'Dashboard',
-            'countUser'=> Pegawai::all()->count(),
-            'countPenjualan'=> Penjualan::all()->count(),
-            'tanggal' => date('M d, Y'),
-            'countCustomer'=> Customer::all()->count(),
-            'countPenjualanBulanan'=> Penjualan::whereMonth('tgl_penjualan', '=', date('m'))->whereYear('tgl_penjualan', '=', date('Y'))->count(),
-            'countPendapatanBulanan'=> Penjualan::whereMonth('tgl_penjualan', '=', date('m'))->whereYear('tgl_penjualan', '=', date('Y'))->sum('total_harga'),
-            'countPendapatan'=> Penjualan::all()->sum('total_harga'),
-            'countProduk'=> Barang::all()->count(),
-            'penjualanperbulan' => $penjualanperbulan,
             
-        ];
-    
-        return view('sales.dashboard',$data);
+            $data = [
+                'title'=>'Dashboard',
+                'countUser'=> Pegawai::all()->count(),
+                'countPenjualan'=> Penjualan::all()->count(),
+                'tanggal' => date('M d, Y'),
+                'countCustomer'=> Customer::all()->count(),
+                'countPenjualanBulanan'=> Penjualan::whereMonth('tgl_penjualan', '=', date('m'))->whereYear('tgl_penjualan', '=', date('Y'))->count(),
+                'countPendapatanBulanan'=> Penjualan::whereMonth('tgl_penjualan', '=', date('m'))->whereYear('tgl_penjualan', '=', date('Y'))->sum('total_harga'),
+                'countPendapatan'=> Penjualan::all()->sum('total_harga'),
+                'countProduk'=> Barang::all()->count(),
+                'penjualanperbulan' => $penjualanperbulan,
+                
+            ];
+        
+            return view('sales.dashboard',$data);
+        }else{
+            return redirect('/login');
+        }
+        
     }
 
     public function index_pimpinan(Request $request){
-        $penjualanperbulan = [];
-        foreach(range(1,12) as $bulan) {
-            $penjualanperbulan[] = Penjualan::whereMonth('tgl_penjualan', '=', date($bulan))->whereYear('tgl_penjualan', '=', date('Y'))->count();
+        if(session()->has('user') && session('user')->jabatan == 'Pimpinan'){
+            $penjualanperbulan = [];
+            foreach(range(1,12) as $bulan) {
+                $penjualanperbulan[] = Penjualan::whereMonth('tgl_penjualan', '=', date($bulan))->whereYear('tgl_penjualan', '=', date('Y'))->count();
+            }
+            $data = [
+                'title'=>'Dashboard',
+                'countUser'=> Pegawai::all()->count(),
+                'countPenjualan'=> Penjualan::all()->count(),
+                'tanggal' => date('M d, Y'),
+                'countCustomer'=> Customer::all()->count(),
+                'countPenjualanBulanan'=> Penjualan::whereMonth('tgl_penjualan', '=', date('m'))->whereYear('tgl_penjualan', '=', date('Y'))->count(),
+                'countPendapatanBulanan'=> Penjualan::whereMonth('tgl_penjualan', '=', date('m'))->whereYear('tgl_penjualan', '=', date('Y'))->sum('total_harga'),
+                'countPendapatan'=> Penjualan::all()->sum('total_harga'),
+                'countProduk'=> Barang::all()->count(),
+                'penjualanperbulan' => $penjualanperbulan,
+                
+            ];
+        
+            return view('pimpinan.dashboard',$data);
+        }else{
+            return redirect('/login');
         }
-        $data = [
-            'title'=>'Dashboard',
-            'countUser'=> Pegawai::all()->count(),
-            'countPenjualan'=> Penjualan::all()->count(),
-            'tanggal' => date('M d, Y'),
-            'countCustomer'=> Customer::all()->count(),
-            'countPenjualanBulanan'=> Penjualan::whereMonth('tgl_penjualan', '=', date('m'))->whereYear('tgl_penjualan', '=', date('Y'))->count(),
-            'countPendapatanBulanan'=> Penjualan::whereMonth('tgl_penjualan', '=', date('m'))->whereYear('tgl_penjualan', '=', date('Y'))->sum('total_harga'),
-            'countPendapatan'=> Penjualan::all()->sum('total_harga'),
-            'countProduk'=> Barang::all()->count(),
-            'penjualanperbulan' => $penjualanperbulan,
-            
-        ];
-    
-        return view('pimpinan.dashboard',$data);
     }
 
     public function index_gudang(Request $request){
-        $penjualanperbulan = [];
-        foreach(range(1,12) as $bulan) {
-            $penjualanperbulan[] = Penjualan::whereMonth('tgl_penjualan', '=', date($bulan))->whereYear('tgl_penjualan', '=', date('Y'))->count();
+        if(session()->has('user') && session('user')->jabatan == 'Gudang'){
+            $penjualanperbulan = [];
+            foreach(range(1,12) as $bulan) {
+                $penjualanperbulan[] = Penjualan::whereMonth('tgl_penjualan', '=', date($bulan))->whereYear('tgl_penjualan', '=', date('Y'))->count();
+            }
+            $data = [
+                'title'=>'Dashboard',
+                'countUser'=> Pegawai::all()->count(),
+                'countPenjualan'=> Penjualan::all()->count(),
+                'tanggal' => date('M d, Y'),
+                'countCustomer'=> Customer::all()->count(),
+                'countPenjualanBulanan'=> Penjualan::whereMonth('tgl_penjualan', '=', date('m'))->whereYear('tgl_penjualan', '=', date('Y'))->count(),
+                'countPendapatanBulanan'=> Penjualan::whereMonth('tgl_penjualan', '=', date('m'))->whereYear('tgl_penjualan', '=', date('Y'))->sum('total_harga'),
+                'countPendapatan'=> Penjualan::all()->sum('total_harga'),
+                'countProduk'=> Barang::all()->count(),
+                'penjualanperbulan' => $penjualanperbulan,
+            ];
+        
+            return view('gudang.dashboard',$data);
+        }else{
+            return redirect('/login');
         }
-        $data = [
-            'title'=>'Dashboard',
-            'countUser'=> Pegawai::all()->count(),
-            'countPenjualan'=> Penjualan::all()->count(),
-            'tanggal' => date('M d, Y'),
-            'countCustomer'=> Customer::all()->count(),
-            'countPenjualanBulanan'=> Penjualan::whereMonth('tgl_penjualan', '=', date('m'))->whereYear('tgl_penjualan', '=', date('Y'))->count(),
-            'countPendapatanBulanan'=> Penjualan::whereMonth('tgl_penjualan', '=', date('m'))->whereYear('tgl_penjualan', '=', date('Y'))->sum('total_harga'),
-            'countPendapatan'=> Penjualan::all()->sum('total_harga'),
-            'countProduk'=> Barang::all()->count(),
-            'penjualanperbulan' => $penjualanperbulan,
-        ];
-    
-        return view('gudang.dashboard',$data);
+        
     }
 }
